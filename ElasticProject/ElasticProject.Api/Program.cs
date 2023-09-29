008d0c71dfe58d90ac0f014775b762469bf6893d
Reza <Reza@DESKTOP-CKL538G>
2023-09-29 22:26:13 +0330

    Install Elasticsearch and set up structure

--- ElasticProject/ElasticProject.Api/Program.cs
+++ ElasticProject/ElasticProject.Api/Program.cs
@@ -1,3 +1,6 @@
+using ElasticProject.Data.Service;
+using ElasticProject.Data.Service.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IElasticsearchService, ElasticsearchService>();
 
 var app = builder.Build();
 
